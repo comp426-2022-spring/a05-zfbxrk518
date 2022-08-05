b53b143c7dd3acf094d5476935e702b36814700f
zfbxrk518 <97485203+zfbxrk518@users.noreply.github.com>
2022-08-05 11:47:54 +0800

    Add files via upload

--- public/main.js
+++ public/main.js
@@ -1,8 +1,85 @@
 // Focus div based on nav button click
-
 // Flip one coin and show coin image to match result when button clicked
+const coin = document.getElementById("coin")
+coin.addEventListener("click", flipCoin)
+async function flipCoin() {
+    const endpoint = "app/flip/"
+    const url = document.baseURI+endpoint
+    await fetch(url)
+  		    .then(function(response) {
+    		    return response.json();
+  		      })
+			    .then(function(result) {
+				    console.log(result);
+				    document.getElementById("result").innerHTML = result.flip;
+				    document.getElementById("quarter").setAttribute("src", "assets/img/"+result.flip+".png");
+				  });
+  };
 
 // Flip multiple coins and show coin images in table as well as summary results
-// Enter number and press button to activate coin flip series
+// Enter number and press button to activate coin flip series 
+const coins = document.getElementById("coins")
+coins.addEventListener("submit", flipCoins)
+async function flipCoins(event) {
+	event.preventDefault();
+	const endpoint = "app/flip/coins/"
+	const url = document.baseURI+endpoint
+	const formEvent = event.currentTarget
+	try {
+		const formData = new FormData(formEvent);
+		const flips = await sendFlips({ url, formData });
+		console.log(flips);
+		document.getElementById("heads").innerHTML = "Heads: "+flips.summary.heads;
+		document.getElementById("tails").innerHTML = "Tails: "+flips.summary.tails;
+    document.getElementById("coinlist").innerHTML = coinList(flips.raw);
+	} catch (error) {
+		console.log(error);
+	}
+}
 
-// Guess a flip by clicking either heads or tails button
+// Guess a coin flip by making a selection and pressing the button.
+const call = document.getElementById("call")
+call.addEventListener("submit", flipCall)
+async function flipCall(event) {
+	event.preventDefault();
+	const endpoint = "app/flip/call/"
+	const url = document.baseURI+endpoint
+	const formEvent = event.currentTarget
+	try {
+		const formData = new FormData(formEvent); 
+		const results = await sendFlips({ url, formData });
+		console.log(results);
+		document.getElementById("choice").innerHTML = "Guess: "+results.call;
+		document.getElementById("actual").innerHTML = "Actual: "+results.flip;
+		document.getElementById("results").innerHTML = "Result: "+results.result;
+    document.getElementById("coingame").innerHTML = '<li><img src="assets/img/'+results.call+'.png" class="bigcoin" id="callcoin"></li><li><img src="assets/img/'+results.flip+'.png" class="bigcoin"></li><li><img src="assets/img/'+results.result+'.png" class="bigcoin"></li>';
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+async function sendFlips({ url, formData }) {
+	const plainFormData = Object.fromEntries(formData.entries());
+	const formDataJson = JSON.stringify(plainFormData);
+	console.log(formDataJson);
+	const options = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			Accept: "application/json"
+		},
+		body: formDataJson
+	};
+	const response = await fetch(url, options);
+	return response.json()
+}
+
+
+function coinList(array) {
+  let text = "";
+  let arrayLength = array.length
+  for (let i = 0; i < arrayLength; i++) {
+    text += '<li><img src="assets/img/'+array[i]+'.png" class="bigcoin"></li>';
+  }
+  return text
+}
